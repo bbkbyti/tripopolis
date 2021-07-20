44705d702c3e83021e3acfa4eb71c1933a842183
Babak Bayati <bbkbyti@Babaks-MacBook-Pro.local>
2021-07-20 08:32:32 -0500

    final version

--- client/src/components/FavoritesDetails.jsx
+++ client/src/components/FavoritesDetails.jsx
@@ -4,45 +4,44 @@ import axios from "axios";
 import { UPLOAD, headers } from "../services";
 
 export default function FavoritesDetails() {
-  const [favorites, setFavorites] = useState({});
-  const { id } = useParams();
-  
-  useEffect(() => {
-    const fetchFavorites = async () => {
-      const URL = `${UPLOAD}/${id}`;
-      const res = await axios.get(URL, { headers });
+    const [favorites, setFavorites] = useState({});
+    const { id } = useParams();
 
-      setFavorites(res.data);
-    };
-    fetchFavorites();
-  }, [id]);
+    useEffect(() => {
+        const fetchFavorites = async () => {
+            const URL = `${UPLOAD}/${id}`;
+            const res = await axios.get(URL, { headers });
 
-  if (!favorites.fields) {
-    return <div>Loading...</div>;
-  }
-  return (
-    <div className=" bg-gray-600 min-h-screen lg:items-center lg:justify-center lg:flex lg:text-2xl overflow-hidden lg:flex-col md:flex md:justify-center md:items-center md:flex-col md:text-2xl">
-      <img
-        className=""
-        src={favorites.fields?.image}
-        alt=" "
-      />
-      <div>
-        <div className="font font-semibold italic text-white">
-          <div className="font-black text-xl mt-5 mb-3">
-            <h1>{favorites.fields?.location}</h1>
-          </div>
-          <div className="uppercase font-bold text-lg">
-            <h2>{favorites.fields?.city}</h2>
-          </div>
-          <div className="uppercase font-bold mb-5">
-            <h3>{favorites.fields?.country}</h3>
-          </div>
-          <div className=" bg-gray-500 font-serif px-6 py-5 text-white  rounded-2xl mr-12 ml-12 transition duration-500 hover:opacity-90 transform ease-in-out hover:-translate-y-1 hover:scale-110">
-            <p>{favorites.fields?.description}</p>
-          </div>
+            setFavorites(res.data);
+        };
+        fetchFavorites();
+    }, [id]);
+
+    if (!favorites.fields) {
+        return <div>Loading...</div>;
+    }
+    return (
+        <div className="  bg-gray-600 min-h-screen overflow-hidden md:flex md:justify-center md:items-center md:flex-col md:text-2xl lg:grid lg:grid-cols-2 lg:min-h-screen ">
+            <img
+                src={favorites.fields?.image}
+                alt=" "
+            />
+            <div>
+                <div className="font-serif font-semibold italic text-white ">
+                    <div className="font-black text-xl mt-5 mb-3 capitalize lg:text-3xl">
+                        <h1>{favorites.fields?.location}</h1>
+                    </div>
+                    <div className="uppercase font-bold text-lg font-serif">
+                        <h2>{favorites.fields?.city}</h2>
+                    </div>
+                    <div className="uppercase font-bold mb-5 font-serif">
+                        <h3>{favorites.fields?.country}</h3>
+                    </div>
+                    <div className=" bg-gray-500 mb-10 font-serif px-6 py-5 text-white rounded-2xl mr-12 ml-12 transition duration-500 hover:opacity-90 transform ease-in-out hover:-translate-y-1 hover:scale-110">
+                        <p>{favorites.fields?.description}</p>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -9,26 +9,26 @@ export default function HomePage() {
         <div className="bg-hero-lg bg-no-repeat h-screen bg-cover overflow-hidden  lg:bg-bbk lg:bg-cover  "  >
 
             <div className="md:mt-40 mt-20 lg:40 justify-around flex flex-col">
-            <div className="bg-gray-700 opacity-90 mt-10 rounded-3xl  md:mx-44 lg:mx-64 h-20 transition duration-500 hover:opacity-90 transform ease-in-out hover:-translate-y-1 hover:scale-110  ">
-                <Link to="/places">
-                    <p className="text-2xl font-serif text-white py-5">All Places</p>
-                </Link>
-            </div>
-            <Link to="/new-place">
-                <div className="bg-gray-700 opacity-90 mt-16 rounded-3xl  md:mx-44 lg:mx-64 h-20 transition duration-500 hover:opacity-90 transform ease-in-out hover:-translate-y-1 hover:scale-110">
-                    <p className="text-2xl font-serif text-white py-5">Share Your Favorite Place</p>
+                <div className="bg-gray-700 opacity-90 mt-10 rounded-3xl  md:mx-44 lg:mx-64 h-20 transition duration-500 hover:opacity-90 transform ease-in-out hover:-translate-y-1 hover:scale-110">
+                    <Link to="/places">
+                        <p className="text-2xl font-serif text-white py-5">All Places</p>
+                    </Link>
                 </div>
-            </Link>
-            <Link to="/favorites">
-            <div className="bg-gray-700 opacity-90 mt-16 rounded-3xl  md:mx-44 lg:mx-64 h-20 transition duration-500 hover:opacity-90 transform ease-in-out hover:-translate-y-1 hover:scale-110">
-                <p className="text-2xl font-serif text-white py-5">My Favorite Places</p>
-            </div>
-            </Link>
-            
+                <Link to="/new-place">
+                    <div className="bg-gray-700 opacity-90 mt-16 rounded-3xl  md:mx-44 lg:mx-64 h-20 transition duration-500 hover:opacity-90 transform ease-in-out hover:-translate-y-1 hover:scale-110">
+                        <p className="text-2xl font-serif text-white py-5">Share Your Favorite Place</p>
+                    </div>
+                </Link>
+                <Link to="/favorites">
+                    <div className="bg-gray-700 opacity-90 mt-16 rounded-3xl  md:mx-44 lg:mx-64 h-20 transition duration-500 hover:opacity-90 transform ease-in-out hover:-translate-y-1 hover:scale-110">
+                        <p className="text-2xl font-serif text-white py-5">My Favorite Places</p>
+                    </div>
+                </Link>
+
 
             </div>
 
-            
+
         </div>
     )
 }
